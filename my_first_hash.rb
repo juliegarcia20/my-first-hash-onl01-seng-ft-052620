a16c2ec05701492f3385aa73dc77e546b66722a0
Julie Garcia <juliegarcia.cali@gmail.com>
2020-06-03 16:30:48 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,1 +1,28 @@
-
+def my_hash
+  pets = {"cat" => "Maru", "dog" => "Pluto"}
+end
+def shipping_manifest
+  the_manifest = {
+               "whale bone corsets" => 5,
+                "porcelain vases" => 2,
+                  "oil paintings" => 3
+                 }
+end
+def retrieval
+  shipping_manifest = {
+              "whale bone corsets" => 5,
+                  "porcelain vases" => 2,
+                  "oil paintings" => 3
+                }
+    shipping_manifest["oil paintings"] = 3
+end
+def adding
+  shipping_manifest = {
+                  "whale bone corsets" => 5,
+                  "porcelain vases" => 2,
+                  "oil paintings" => 3
+                }
+ shipping_manifest["muskets"] = 2
+ shipping_manifest["gun powder"] = 4
+  return shipping_manifest
+ end
